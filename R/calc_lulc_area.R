269fbc4004ab64115b9dc8b104cfc4bad19ec4f6
Hugo Seixas <hugo.seixas@alumni.usp.br>
2021-02-01 09:52:16 -0300

    Formated function in a package style

--- R/calc_lulc_area.R
+++ R/calc_lulc_area.R
@@ -1,22 +1,27 @@
 
 #' @export
-calc_area <- function(image, features) {
+calc_lulc_area <- function(image, features, scale = 30) {
 
   bands <- image$bandNames()$getInfo()
 
+  features <-
+    features %>%
+    sf::st_as_sf() %>%
+    dplyr::mutate(id = row_number()) %>%
+    dplyr::rename(geom = x)
+
   # Calculate area for each value, band, and municipality
   area_collection <-
-    map_df(
-      .x = municip_intersection$code_muni,
-      function(muni) {
+    map2_df(
+      .x = features$geom,
+      .y = features$id,
+      function(geometry, id) {
 
-        cat("\n", muni, "\n")
+        cat("\n", id, " ")
 
-        ## Load one state into GEE ----
-        ee_state <-
-          sf_as_ee(
-            municip_intersection %>% filter(code_muni == muni)
-          )
+        # Load one feature into GEE
+        ee_geometry <-
+          rgee::sf_as_ee(geometry)
 
         # Get area for each band in the state
         return(
@@ -24,20 +29,20 @@ calc_area <- function(image, features) {
             .x = bands,
             function(b) {
 
-              cat(b, " ", sep = " ")
+              cat(".")
 
               # Calculate area by group (discrete values of each band)
               areas <-
                 ee$Image$
                 pixelArea()$
-                addBands(img$select(b))$
+                addBands(image$select(b))$
                 reduceRegion(
                   reducer = ee$Reducer$sum()$group(
                     groupField = 1L,
                     groupName = "value"
                   ),
-                  geometry = ee_state$first()$geometry(),
-                  scale = 30,
+                  geometry = ee_geometry,
+                  scale = scale,
                   maxPixels = 1e13
                 )$
                 get("groups")$
@@ -45,9 +50,12 @@ calc_area <- function(image, features) {
 
               # Return values as a table
               return(
-                tibble(key = map(areas, "value"), area = map(areas, "sum")) %>%
-                  unnest(cols = c(key, area)) %>%
-                  mutate(muni_code = muni, layer = b)
+                tibble::tibble(
+                  key = map(areas, "value"),
+                  area = map(areas, "sum")
+                ) %>%
+                  tidyr::unnest(cols = c(key, area)) %>%
+                  dplyr::mutate(id = id, layer = b)
               )
 
             }
